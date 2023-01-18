5763bbfab79bb8c4c98eb90993fabcef9cbe0782
RemyaAlphonsa <114404997+RemyaAlphonsa@users.noreply.github.com>
2023-01-19 00:56:00 +0530

    Pangram

---
+++ string/AssignQ4_Pangram.java
@@ -0,0 +1,19 @@
+package string;
+
+public class AssignQ4_Pangram {
+
+	public static void main(String[] args) {
+		String str = "abcdefghijklmnopqrstuvwxyz";
+		char arr1[] = str.toCharArray();
+		
+		String strUser = "hjk";
+		char arr2[] = strUser.toCharArray();
+		char arr3[];
+		
+		for(int i=0;i<arr2.length;i++) {
+			
+		}
+
+	}
+
+}
