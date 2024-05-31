2637990fc5a2e0657a598185061893d86be61dc5
RemyaAlphonsa <114404997+RemyaAlphonsa@users.noreply.github.com>
2024-05-31 19:50:00 +0530

    Update AssignQ4_Pangram.java

--- string/AssignQ4_Pangram.java
+++ string/AssignQ4_Pangram.java
@@ -3,17 +3,29 @@ package string;
 public class AssignQ4_Pangram {
 
 	public static void main(String[] args) {
-		String str = "abcdefghijklmnopqrstuvwxyz";
-		char arr1[] = str.toCharArray();
 		
-		String strUser = "hjk";
-		char arr2[] = strUser.toCharArray();
-		char arr3[];
-		
-		for(int i=0;i<arr2.length;i++) {
-			
+		String name = "I am a boy";
+		String check = "The quick brown fox jumps over the lazy dog";
+		String alph = "abcdefghijklmnopqrstuvwxyz";
+		Boolean result = isPangram(alph.toLowerCase());
+		if(result) {
+			System.out.println("is Pangram");
+		}else {
+			System.out.println("is not pangram");
 		}
-
 	}
+	public static boolean isPangram(String str) {
+		
 
+		if(str.length() < 26) {
+           return false;
+		}else {
+			for(char ch='a';ch<='z';ch++) {
+				if(str.toLowerCase().indexOf(ch)< 0) {
+					return false;		
+				}
+			}
+		   return true;
+	    }
+	}
 }
